e9358b8b0e852d886fbad98df8e318f89369d897
Anton Deguet <anton.deguet@jhu.edu>
2024-10-14 14:43:03 -0400

    Added include cstdint

--- core/components/include/sawGalilController/mtsGalilController.h
+++ core/components/include/sawGalilController/mtsGalilController.h
@@ -40,6 +40,7 @@ http://www.cisst.org/cisst/license.txt.
 #define _mtsGalilController_h
 
 #include <string>
+#include <cstdint>
 
 #include <cisstVector/vctDynamicVectorTypes.h>
 #include <cisstMultiTask/mtsTaskContinuous.h>
